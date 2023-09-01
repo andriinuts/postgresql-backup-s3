4eefd938ae5a881fd6664be26ba0b5ff4dc49826
Andrii Nutskovskyi <andrijn92@gmail.com>
2023-09-01 13:11:05 +0000

    Add log

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ if [ "${S3_S3V4}" = "yes" ]; then
     aws configure set default.s3.signature_version s3v4
 fi
 
+echo SCHEDULE value: ${SCHEDULE}"
+
 if [ "${SCHEDULE}" = "**None**" ]; then
   sh backup.sh
 else
